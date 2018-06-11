ea9d3946b3219ce88a85aab4eb1fefe224f4f19a
george <jqnk124@gmail.com>
2018-06-11 23:14:54 +0800

    prompt as begin

---
+++ src/prompt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
+
+int main (int argc, char ** argv) {
+    puts("Lispq Version 0.0.1");
+	puts("Press Ctl-c to Exit\n");
+
+	while(1) {
+        char * input = readline("lispq > ");
+
+		add_history(input);
+
+	    printf("No you're a %s\n", input);
+
+		free(input);	
+	}
+	return 0;
+}
